1e04da4b37b6b0ec664d0c249bd36d8340265d38
Eustaquio Junior <emjr7l@gmail.com>
2024-03-29 15:44:21 -0300

    Classe Variaveis criada

---
+++ src/estudosjava/variaveis/Variaveis.java
@@ -0,0 +1,17 @@
+package estudosjava.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//Em Java temos 8 tipos primitivos de variáveis
+		
+		//tipos primitivos de verdade sao os boolean, com valores true ou false
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println("Legítimos Primitivos, são os boolean!");
+		System.out.println("Seus valores podem ser: " +verdadeiro +" ou " +falso);
+	}
+
+}
