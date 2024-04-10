1b777473991f39ce8c4f016cb894c4ba31afe2dc
Eustaquio Junior <emjr7l@gmail.com>
2024-04-10 19:45:50 -0300

    adicionados todos os tipos inteiros à classe Variaveis

--- src/estudosjava/variaveis/Variaveis.java
+++ src/estudosjava/variaveis/Variaveis.java
@@ -12,18 +12,35 @@ public class Variaveis {
 		
 		System.out.println("Legítimos Primitivos, são os boolean!");
 		System.out.println("Seus valores podem ser: " +verdadeiro +" ou " +falso);
+		System.out.println(" ");
 		
 		byte maxnegativobyte = -128;
 		byte maxpositivobyte = 127;
 		
-		System.out.println("Variáveis byte como o nome diz, ocupam 01 byte de memórria");
+		System.out.println("Variáveis byte como o nome diz, ocupam 01 byte, ou 08 bits de memória");
 		System.out.println("Aceitam valores inteiros entre " +maxnegativobyte +" e " +maxpositivobyte);
+		System.out.println(" ");
 		
 		short maxnegativoshort = -32768;
 		short maxpositivoshort = 32767;
 		
-		System.out.println("Variáveis short ocupam 02 bytes de memórria");
+		System.out.println("Variáveis short ocupam 02 bytes, ou 16 bits de memória");
 		System.out.println("Aceitam valores inteiros entre " +maxnegativoshort +" e " +maxpositivoshort);
+		System.out.println(" ");
+		
+		int maxnegativoint = -2147483648;
+		int maxpositivoint = 2147483647;
+		
+		System.out.println("Variáveis int ocupam 04 bytes, ou 32 bits de memória");
+		System.out.println("Aceitam valores inteiros entre " +maxnegativoint +" e " +maxpositivoint);
+		System.out.println(" ");
+		
+		long maxnegativolong = -9223372036854775808l;
+		long maxpositivolong = 9223372036854775807l;
+		
+		System.out.println("Variáveis long ocupam 08 bytes, ou 64 bits de memória");
+		System.out.println("Aceitam valores inteiros entre " +maxnegativolong +" e " +maxpositivolong);
+		System.out.println(" ");
 	}
 
 }
